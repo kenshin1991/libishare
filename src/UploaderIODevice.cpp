56958ed7854376f1d80d4ca77104e6edb63df5ab
Rohit Yadav <rohityadav89@gmail.com>
2010-07-03 01:43:47 +0200

    init parent class

--- src/UploaderIODevice.cpp
+++ src/UploaderIODevice.cpp
@@ -24,6 +24,7 @@
 
 UploaderIODevice::UploaderIODevice( QObject *parent, const QString& fileName,
                                    const QByteArray& head, const QByteArray& tail )
+                                   : QIODevice( parent )
 {
     m_file = new QFile( fileName, this );
     m_head = new QByteArray( head );
